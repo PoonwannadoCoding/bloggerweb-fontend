d92a72f2fdba1ddd3b0d89c9fb92d4f6782ad1e3
Worawit Penglam <82023896+PoonwannadoCoding@users.noreply.github.com>
2024-03-24 01:56:40 +0700

    make component aka widget as navbar

--- src/Screen/HomePage.vue
+++ src/Screen/HomePage.vue
@@ -1,76 +1,27 @@
-<script>
-    import 'primeicons/primeicons.css'
-</script>
+
 
 <template>
+    <NavBar />
+  </template>
 
-    <div class="app-bar">
-        <nav class="navbar">
-            <div class="logo">
-                <i class="pi pi-book" ></i>
-            </div>
 
-            <div class="app-name">
-                BLOGGER
-            </div>
-            
-            <div class="search-bar-container">
-                <input class="search-bar" v-model="text">
-            </div>
 
-            <div class="user-icon">
-                <i class="pi pi-user" ></i>
-            </div>
-        </nav>
-    </div>
-</template>
+<script>
+    import NavBar from "../widgets/navbar.vue"
+
+    export default{
+        components:{
+            NavBar
+        }
+    }
+
+</script>
 
 <style>
 
-nav {
-        background-color: #C8EAD1;
-        height: 60px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 10px; 
-    }
-
-    .app-bar {
+body {
+        background-color: #31363F;
         margin: 0;
         padding: 0;
     }
-
-    .search-bar-container {
-        flex: 1;
-        display: flex;
-        justify-content: center; 
-    }
-
-    .search-bar {
-        width: 100%;
-        max-width: 500px;
-        height: 25px;
-        border-radius: 50px;
-
-    }
-
-    .user-icon {
-        color: black;
-        font-size: 30px;
-        margin-right: 10px;
-    }
-
-    .logo {
-        color: black;
-        font-size: 30px;
-        margin-left: 10px;
-    }
-
-    .app-name{
-        font-size: 30px;
-        margin-left: 15px;
-    }
-
-
 </style>
---
+++ src/widgets/navbar.vue
@@ -0,0 +1,82 @@
+
+
+<template>
+    <div class="app-bar">
+        <nav class="navbar">
+            <div class="logo">
+                <i class="pi pi-book" ></i>
+            </div>
+
+            <div class="app-name">
+                BLOGGER
+            </div>
+            
+            <div class="search-bar-container">
+                <input class="search-bar" v-model="text">
+            </div>
+
+            <div class="user-icon">
+                <i class="pi pi-user" ></i>
+            </div>
+        </nav>
+    </div>
+</template>
+
+
+<script>
+    import 'primeicons/primeicons.css'
+    export default {
+        name: 'NavBar',
+    }
+</script>
+
+
+<style>
+
+nav {
+        background-color: #222831;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 10px; 
+    }
+
+    .app-bar {
+        margin: 0;
+        padding: 0;
+    }
+
+    .search-bar-container {
+        flex: 1;
+        display: flex;
+        justify-content: center; 
+    }
+
+    .search-bar {
+        width: 100%;
+        max-width: 500px;
+        height: 25px;
+        border-radius: 50px;
+
+    }
+
+    .user-icon {
+        color: white;
+        font-size: 30px;
+        margin-right: 10px;
+    }
+
+    .logo {
+        color: white;
+        font-size: 30px;
+        margin-left: 10px;
+    }
+
+    .app-name{
+        color:white;
+        font-size: 30px;
+        margin-left: 15px;
+    }
+
+</style>
