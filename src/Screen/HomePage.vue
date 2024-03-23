3b4f6c1cada1e6cb56e2cd40f89cbc328790e233
Worawit Penglam <82023896+PoonwannadoCoding@users.noreply.github.com>
2024-03-24 01:26:45 +0700

    create appbar

---
+++ src/Screen/HomePage.vue
@@ -0,0 +1,76 @@
+<script>
+    import 'primeicons/primeicons.css'
+</script>
+
+<template>
+
+    <div class="app-bar">
+        <nav class="navbar">
+            <div class="logo">
+                <i class="pi pi-book" ></i>
+            </div>
+
+            <div class="app-name">
+                BLOGGER
+            </div>
+            
+            <div class="search-bar-container">
+                <input class="search-bar" v-model="text">
+            </div>
+
+            <div class="user-icon">
+                <i class="pi pi-user" ></i>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<style>
+
+nav {
+        background-color: #C8EAD1;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 10px; 
+    }
+
+    .app-bar {
+        margin: 0;
+        padding: 0;
+    }
+
+    .search-bar-container {
+        flex: 1;
+        display: flex;
+        justify-content: center; 
+    }
+
+    .search-bar {
+        width: 100%;
+        max-width: 500px;
+        height: 25px;
+        border-radius: 50px;
+
+    }
+
+    .user-icon {
+        color: black;
+        font-size: 30px;
+        margin-right: 10px;
+    }
+
+    .logo {
+        color: black;
+        font-size: 30px;
+        margin-left: 10px;
+    }
+
+    .app-name{
+        font-size: 30px;
+        margin-left: 15px;
+    }
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import HomePage from './Screen/HomePage.vue'
+
+
+createApp(HomePage).mount('#app')
